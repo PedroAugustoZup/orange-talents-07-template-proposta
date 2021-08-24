8f8dd86fa5d13b6ce40b8db3b7c83b1af750e4fd
Pedro Augusto <pedro.marques@zup.com.br>
2021-08-24 16:33:57 -0300

    Utilizando o Jaeger

--- src/main/java/br/com/zupacademy/proposta/repository/CartaoRepository.java
+++ src/main/java/br/com/zupacademy/proposta/repository/CartaoRepository.java
@@ -2,6 +2,8 @@ package br.com.zupacademy.proposta.repository;
 
 import br.com.zupacademy.proposta.model.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 }
